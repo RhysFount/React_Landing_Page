f6f8b8ede8d83187e501ff0f6d65908a30ee9aad
RhysFount <Rfountain@hotmail.co.uk>
2025-09-25 15:05:25 +0100

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const colourArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+  let time; 
+  
 
   return (
     <>
@@ -25,7 +28,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to my Vite and React Landing Page</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -37,6 +40,26 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="card">
+        <p>Click here to move this button.</p>
+        <button id="movingbtn" onClick={() => document.getElementById("movingbtn").style.transform = `TranslateX(${Math.floor(Math.random(0,50)*50)}%) TranslateY(${Math.floor(Math.random(0,50)*50)}%) `}>
+          Move me
+        </button>
+        <p>This is the websites first Card.</p>
+      </div>
+      <div className="card">
+        <p>Click here to display the date time.</p>
+        <button onClick={() => time => Date().slice(15, 25)}>
+          The current time is: {time}
+        </button>
+        <p>This is the websites second Card.</p>
+      </div>
+      <div className="card">
+        <p>This is the websites third Card.</p>
+      </div>
+      <div className="footer">
+        <p>This website was designed by Rhys Fountain.</p>
+      </div>
     </>
   )
 }
